fd1770224bd68940b2cf2698dc5f4e1f2721a025
William Ravelo <drummerwilliam@gmail.com>
2018-07-06 11:50:42 -0500

    finished assignment

--- InversionCounter/InversionCounter.java
+++ InversionCounter/InversionCounter.java
@@ -1,30 +1,41 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.File;
+
 public class InversionCounter {
 
-	public static void mergeSort(int[] array) {
-		_mergeSort(array, 0, array.length-1);
+	public static int mergeSort(int[] array) {
+		int ans = _mergeSort(array, 0, array.length-1);
+		return ans;
 	}
 
-	public static void _mergeSort(int array[], int start, int end) {
+	public static int _mergeSort(int array[], int start, int end) {
 		int middle = (start+end)/2;
 
+		int inv = 0;
 		if(start < end) {
-			_mergeSort(array, start, middle);
-			_mergeSort(array, middle+1, end);
-			merge(array, start, end);
+			inv += _mergeSort(array, start, middle);
+			inv += _mergeSort(array, middle+1, end);
+			inv += merge(array, start, end);
+			return inv;
 		}
+
+		return inv;
 	}
 
-	public static void merge(int array[], int start, int end) {
+	public static int merge(int array[], int start, int end) {
 		int middle = (start+end)/2;
 		int[] newArray = new int[end-start+1];
 		int i = start;
 		int j = middle+1;
 		int k = 0;
+		int inv = 0;
 
 		while(i <= middle && j <= end) {
 			if(array[i] <= array[j]) {
 				newArray[k++] = array[i++];
 			} else {
+				inv += (j-i);
 				newArray[k++] = array[j++];
 			}
 		}
@@ -40,15 +51,34 @@ public class InversionCounter {
 		for(i = 0; i < newArray.length; i++) {
 			array[i+start] = newArray[i];
 		}
+
+		return inv;
 	}
 
+	public static int[] fileToIntArray(String fileName) {
+		try {
+			Scanner sc = new Scanner(new File(fileName));
+			ArrayList<String> sts = new ArrayList<>();
+
+			while(sc.hasNextLine()) {
+				String line = sc.nextLine();
+				sts.add(line);
+			}
+			int[] ans = new int[sts.size()];
+			for(int i = 0; i < ans.length; i++) {
+				ans[i] = Integer.parseInt(sts.get(i));	
+			}
+
+			return ans;
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return new int[0];
+	}
 
 	public static void main(String args[]) {
-		int[] array = {3,5,2,5,7,3,2,1};
-		mergeSort(array);
-		System.out.println("Termino\n");
-		for(int x: array) {
-			System.out.print(x +" ");
-		}
+		int[] array = fileToIntArray("./test.txt");
+		int ans = mergeSort(array);
+		System.out.println(ans);
 	}
 }
